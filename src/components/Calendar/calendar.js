b3800452d92ffd10e8dc031ae26f8d439a652ef6
Lesnov Artem <lesnovart88@gmail.com>
2020-12-06 16:51:38 +0200

    make structure

---
+++ src/components/Calendar/calendar.js
@@ -0,0 +1,34 @@
+import { Component } from "..";
+import { Navigation } from "./navigation";
+import { Table } from "./table";
+
+export class Calendar extends Component {
+  constructor(parentSelector) {
+    super(parentSelector);
+    this.currentDate = new Date();
+    this.component.classList.add("calendar");
+    this.table = new Table(this.component, this.currentDate);
+    this.nav = new Navigation(this.component, this.currentDate, this.table);
+  }
+  render() {
+    super.render();
+    this.nav.render();
+    this.table.render();
+  }
+
+
+
+
+  // createCurrentLabel() {
+  //   this.component.prepend(this.label.component);
+  //   this.updateCurrentLabel(this.currentDate);
+  // }
+
+  // updateCurrentLabel(date) {
+  //   const { component: label } = this.label;
+  //   label.textContent = dateFormatter
+  //     .format(new Date(date))
+  //     .replace(",", "")
+  //     .split(" ")[1];
+  // }
+}
