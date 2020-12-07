3d04f0471c27e6851de8b1880931dc2372d02bbf
Lesnov Artem <lesnovart88@gmail.com>
2020-12-07 12:40:13 +0200

    add prev/next persent and fixed bugs

--- src/components/Calendar/teamRowComponent.js
+++ src/components/Calendar/teamRowComponent.js
@@ -6,31 +6,59 @@ import { TeamSum } from "./teamSumComponent";
 export class TeamRowComponent extends Component {
   constructor(parentSelector, depTeamInfo, monthLength, date) {
     super(parentSelector, "tr");
-    this.teamMembers = [];
     this.date = date;
     this.monthLength = monthLength;
     this.depTeamInfo = depTeamInfo;
     this.component.className = `mainRow ${this.depTeamInfo.name
       .split(" ")[0]
       .toLowerCase()}`;
+    this.daysContext = [];
   }
 
   generateTeamHeader() {
     const teamName = new TeamName(this.component, this.depTeamInfo, this.date);
-    this.component.append(teamName);
+    this.component.append(teamName.component);
 
-    for (let index = 0; index < this.monthLength; index++) {
+    for (let index = 0; index < 31; index++) {
       const teamCell = new TeamCell(
         this.component,
         this.depTeamInfo,
         this.monthLength,
         this.date,
       );
-      this.component.append(teamCell);
+      this.daysContext.push(teamCell);
+      this.component.append(teamCell.component);
+      if (index >= this.monthLength) {
+        teamCell.hide();
+      }
     }
-
     const teamSum = new TeamSum();
-    this.component.append(teamSum);
+    this.component.append(teamSum.component);
+  }
+
+  updateTeamHeader(newDate) {
+    const daysInPreviousMonth = this.monthLength;
+    this.monthLength = new Date(
+      newDate.getFullYear(),
+      newDate.getMonth() + 1,
+      0,
+    ).getDate();
+
+    const percentageOfAbsentData = this.daysContext[0].depTeamInfo
+      .percentageOfAbsent;
+    const percent = this.component.querySelector(".percent");
+    const currentMonth = newDate.getMonth();
+    percent.textContent = `${percentageOfAbsentData[currentMonth]} %`;
+
+    if (this.monthLength < daysInPreviousMonth) {
+      for (let index = this.monthLength; index < daysInPreviousMonth; index++) {
+        this.daysContext[index].hide();
+      }
+    } else if (this.monthLength > daysInPreviousMonth) {
+      for (let index = daysInPreviousMonth; index < this.monthLength; index++) {
+        this.daysContext[index].show();
+      }
+    }
   }
 
   render() {
