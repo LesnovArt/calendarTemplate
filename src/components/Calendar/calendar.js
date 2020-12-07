9685643e290001a7d289fb1aad713da93456dcfe
Lesnov Artem <lesnovart88@gmail.com>
2020-12-07 12:48:44 +0200

    change index/import/add depteams.js

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,34 +1,41 @@
-import { Component } from "..";
-import { Navigation } from "./navigation";
-import { Table } from "./table";
+import {
+  Component
+} from "..";
+import {
+  Navigation
+} from "./navigation";
+import {
+  Table
+} from "./table";
+import {
+  PopupWindow
+} from "../PopupWindow";
+import {
+  VacationForm
+} from "../VacationForm";
+
 
 export class Calendar extends Component {
   constructor(parentSelector) {
     super(parentSelector);
     this.currentDate = new Date();
     this.component.classList.add("calendar");
-    this.table = new Table(this.component, this.currentDate);
+    this.popup = new PopupWindow("#app");
+    this.popupForm = new VacationForm(this.popup.component);
+    this.table = new Table(this.component, this.currentDate, this.popupForm);
     this.nav = new Navigation(this.component, this.currentDate, this.table);
+
+
+
   }
   render() {
     super.render();
     this.nav.render();
     this.table.render();
+    this.popup.render();
+    if (!this.popup.state.loading && !this.popup.state.error) {
+      this.popupForm.render()
+    }
+
   }
-
-
-
-
-  // createCurrentLabel() {
-  //   this.component.prepend(this.label.component);
-  //   this.updateCurrentLabel(this.currentDate);
-  // }
-
-  // updateCurrentLabel(date) {
-  //   const { component: label } = this.label;
-  //   label.textContent = dateFormatter
-  //     .format(new Date(date))
-  //     .replace(",", "")
-  //     .split(" ")[1];
-  // }
 }
