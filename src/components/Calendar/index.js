5564a795b26f1129c42e42b9684f94fd2b353357
Lesnov Artem <lesnovart88@gmail.com>
2020-12-07 23:27:05 +0200

    fixed team-component import

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,7 +1,4 @@
 export { Calendar } from "./calendar";
 export { Navigation } from "./navigation";
 export { Table } from "./table";
-export { TeamRowComponent } from "./teamRowComponent";
-export { TeamName } from "./teamNameComponent";
-export { TeamCell } from "./teamCellComponent";
-export { TeamSum } from "./teamSumComponent";
+
