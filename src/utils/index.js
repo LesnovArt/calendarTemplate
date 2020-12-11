92d5e3af3c298fa56eba4918b2f8186edbfb93a6
Lesnov_Art <lesnovart88@gmail.com>
2020-12-11 17:14:59 +0200

    start testings

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,2 @@
 export { dateFormatter, formatInputDate } from "./dateUtils";
-<<<<<<< HEAD
 export { departmentTeams } from "./departmentTeams";
-=======
-export { departmentTeams } from "./departmentTeams";
->>>>>>> teams-component
